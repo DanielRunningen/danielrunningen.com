0bd74fcdb5a49c1b4a32fb5c1bf310b116671182
Daniel Runningen <daniel.runningen+github@outlook.com>
2021-08-25 14:28:32 -0500

    Adding specific genre pages

---
+++ pages/gallery/_slug.vue
@@ -0,0 +1,21 @@
+<template>
+   <main>
+      <div class="p-5">
+         <h1>{{ g.title }}</h1>
+         <nuxt-content :document="{ body: g.body }" />
+      </div>
+      <Carousel :content="g.data" parentSlug="gallery" />
+   </main>
+</template>
+
+<script>
+export default {
+   async asyncData({ $content, params }) {
+      const g = await $content('gallery', params.slug).fetch();
+
+      return {
+         genre: g,
+      };
+   },
+};
+</script>
