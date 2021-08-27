aa747ab29df38eb3bef32c0646564ec84b78c7e6
Daniel Runningen <daniel.runningen+github@outlook.com>
2021-08-27 10:53:06 -0500

    Adding slug-specific metadata

--- pages/projects/_slug.vue
+++ pages/projects/_slug.vue
@@ -11,5 +11,46 @@ export default {
          project: p,
       };
    },
+   head() {
+      const meta = {
+         title: this.project.title,
+         description: this.project.description,
+         image:
+            process.env.baseUrl + require(`~/assets/img/${this.project.image}`),
+      };
+      return {
+         title: meta.title,
+         meta: [
+            {
+               hid: 'description',
+               name: 'description',
+               content: meta.description,
+            },
+            { hid: 'og:title', name: 'og:title', content: meta.title },
+            { hid: 'og:image', name: 'og:image', content: meta.image },
+            { hid: 'og:site_name', name: 'og:site_name', content: meta.title },
+            {
+               hid: 'og:description',
+               name: 'og:description',
+               content: meta.description,
+            },
+            {
+               hid: 'twitter:description',
+               name: 'twitter:description',
+               content: meta.description,
+            },
+            {
+               hid: 'twitter:title',
+               name: 'twitter:title',
+               content: meta.title,
+            },
+            {
+               hid: 'twitter:image',
+               name: 'twitter:image',
+               content: meta.image,
+            },
+         ],
+      };
+   },
 };
 </script>
