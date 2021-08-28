f5777f5481d35cde54ffaa9a4c15c3152fac9e7f
Daniel Runningen <daniel.runningen+github@outlook.com>
2021-08-28 11:22:42 -0500

    Updating styles for blockquote

--- components/Quote.vue
+++ components/Quote.vue
@@ -1,19 +1,29 @@
 <template>
-   <figure class="bg-primary text-secondary p-4 rounded">
-      <svg class="fill-secondary" height="25" viewBox="0 0 7 5">
+   <figure class="bg-primary text-secondary my-5 px-4 pt-4 pb-3 rounded">
+      <svg
+         class="fill-secondary"
+         style="opacity: 0.5"
+         height="25"
+         viewBox="0 0 7 5"
+      >
          <polygon points="2,0 4,0 1,5 0,5" />
          <polygon points="5,0 7,0 4,5 3,5" />
       </svg>
       <blockquote class="blockquote px-5">
          {{ text }}
       </blockquote>
-      <svg class="fill-secondary d-block ms-auto" height="25" viewBox="0 0 7 5">
+      <figcaption class="blockquote-footer mb-0 mx-3 px-5">
+         <slot />
+      </figcaption>
+      <svg
+         class="fill-secondary d-block ms-auto mb-2"
+         style="opacity: 0.5"
+         height="25"
+         viewBox="0 0 7 5"
+      >
          <polygon points="2,0 4,0 1,5 0,5" />
          <polygon points="5,0 7,0 4,5 3,5" />
       </svg>
-      <figcaption class="blockquote-footer">
-         <slot />
-      </figcaption>
    </figure>
 </template>
 
