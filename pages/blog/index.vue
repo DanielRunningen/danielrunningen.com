bed830ae1b97c03df6e051e639603e029527cea7
Daniel Runningen <DanielRunningen@users.noreply.github.com>
2022-07-28 22:09:41 +0000

    Adding Blog (#3)
    * Enabling the blog section
    
    * Posting Blog

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,22 +1,62 @@
 <template>
-   <main>
-      <div class="h1 text-center bg-secondary p-5 m-0">Coming soon!</div>
+   <main class="p-4">
+      <h1 class="text-center">Blog Posts</h1>
+      <div class="row g-3">
+         <ContentCard v-for="p in posts" :key="p.slug" :content="p" />
+      </div>
    </main>
 </template>
 
 <script>
-const meta = {
-   title: "Dan's Blog",
-};
-
 export default {
-   head: {
-      title: meta.title,
-      meta: [
-         { hid: 'og:title', name: 'og:title', content: meta.title },
-         { hid: 'og:site_name', name: 'og:site_name', content: meta.title },
-         { hid: 'twitter:title', name: 'twitter:title', content: meta.title },
-      ],
+   async asyncData({ $content }) {
+      const p = await $content('blog').sortBy('createdAt', 'desc').fetch();
+
+      return {
+         posts: p,
+      };
+   },
+   head() {
+      const meta = {
+         title: "Dan's Blog",
+         description: this.posts[0].description,
+         image:
+            process.env.baseUrl +
+            require(`~/assets/img/${this.posts[0].image}`),
+      };
+      return {
+         title: meta.title,
+         meta: [
+            {
+               hid: 'description',
+               name: 'description',
+               content: meta.description,
+            },
+            { hid: 'og:title', name: 'og:title', content: meta.title },
+            { hid: 'og:image', name: 'og:image', content: meta.image },
+            { hid: 'og:site_name', name: 'og:site_name', content: meta.title },
+            {
+               hid: 'og:description',
+               name: 'og:description',
+               content: meta.description,
+            },
+            {
+               hid: 'twitter:description',
+               name: 'twitter:description',
+               content: meta.description,
+            },
+            {
+               hid: 'twitter:title',
+               name: 'twitter:title',
+               content: meta.title,
+            },
+            {
+               hid: 'twitter:image',
+               name: 'twitter:image',
+               content: meta.image,
+            },
+         ],
+      };
    },
 };
 </script>
